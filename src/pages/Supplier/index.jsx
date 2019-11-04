026f11dd901bfffc565f1dbdf2b09027d49cdb83
kimthunbunly <kimthunbunly.rupp@gmail.com>
2019-11-04 21:11:52 +0700

    fixed request not stop

--- src/pages/Supplier/index.jsx
+++ src/pages/Supplier/index.jsx
@@ -1,14 +1,36 @@
-import React from "react";
-import Table from "../../components/Table";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import ENV from "../../config/config.json";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Table from "../../components/Table";
+import face18Jpg from "../../assets/images/faces/face18.jpg";
 
-function Supplier() {
+function Suppiler(props) {
+  const a = [1, 2, 3, 4, 5, 6];
+  const [tableData, setTableData] = useState([]);
+  async function callUserApi() {
+    const response = await axios.get(ENV.API_ENDPOINT + "suppiler", {
+      headers: {
+        "x-store": localStorage.getItem(ENV.APP_TOKEN)
+      }
+    });
+    return response;
+  }
+  useEffect(() => {
+    callUserApi().then(response => {
+      console.log(response.data.payload);
+      setTableData(response.data.payload);
+      //   console.log(tableData);
+    });
+    // console.log(data);
+  },[]);
   return (
     <div className="content-wrapper">
       <div className="page-header">
         <h3 className="page-title">
           <span className="page-title-icon bg-gradient-primary text-white mr-2">
-            <div className="d-flex h-100">
+          <div className="d-flex h-100">
               <FontAwesomeIcon
                 className="align-self-center mx-auto "
                 icon="people-carry"
@@ -17,10 +39,10 @@ function Supplier() {
               </FontAwesomeIcon>
             </div>
           </span>
-          Supplier{""}
-          <a href="/admin/user/create">
+          Suppiler{""}
+          <a href="/admin/Suppiler/create">
             <button className="btn btn-outline-primary ml-2">
-              View Supplier
+              Create Suppiler
             </button>
           </a>
         </h3>
@@ -28,7 +50,7 @@ function Supplier() {
         <nav aria-label="breadcrumb">
           <ul className="breadcrumb">
             <li className="breadcrumb-item active" aria-current="page">
-              <span></span> 
+              <span></span> Users{" "}
             </li>
           </ul>
         </nav>
@@ -37,12 +59,138 @@ function Supplier() {
         <div className="col-xl-8 col-lg-7 col-4 grid-margin">
           <div className="card">
             <div className="card-body">
-              <h1 className="card-title">All Supplier</h1>
+              <h1 className="card-title">All Suppilers</h1>
               <div className="table-responsive">
-                <Table
-                  header={["ID", "Product", "Quantity","Type"]}
-                  apiEndpoint="stock"
-                ></Table>
+                <Table header={["ID", "Name", "Role"]} apiEndpoint="user">
+                  {tableData.map(data => (
+                    <tr key={data._id}>
+                      <td>{data._id}</td>
+                      <td>{data.username}</td>
+                      <td>{data.role}</td>
+                    </tr>
+                  ))}
+                </Table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-4 col-lg-5 col-5 grid-margin">
+          <div className="card">
+            <div className="card-body">
+              <div className="d-flex flex-row">
+                <div
+                  style={{
+                    backgroundImage: `url(${face18Jpg})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    margin: "0",
+                    padding: "0",
+                    height: "5rem",
+                    width: "5rem",
+                    content: "",
+                    borderRadius: "50%",
+                    marginRight: "2rem"
+                  }}
+                ></div>
+                <div>
+                  <h3>Alice Eve</h3>
+                  <h5>Project Manager</h5>
+                </div>
+              </div>
+              <div className="pt-3">
+                <div>
+                  <hr />
+                  <h5>Email</h5>
+                  <p className="pl-1">aliceeve@gmail.com</p>
+                </div>
+                <div>
+                  <hr />
+                  <h5>Phone Number</h5>
+                  <p className="pl-1">+(855) 012 554 665</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-4">
+          <div className="card">
+            <div className="card-body">
+              <div className="d-flex flex-row">
+                <div>
+                  <h3>All Managers</h3>
+                  <h5>Total 5 Users</h5>
+                </div>
+              </div>
+              <div className="pt-3">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Picture</th>
+                      <th>Name</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>001</td>
+                      <td>icon</td>
+                      <td>Jacob</td>
+                    </tr>
+                    <tr>
+                      <td>001</td>
+                      <td>icon</td>
+                      <td>Jacob</td>
+                    </tr>
+                    <tr>
+                      <td>001</td>
+                      <td>icon</td>
+                      <td>Jacob</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card">
+            <div className="card-body">
+              <div className="d-flex flex-row">
+                <div>
+                  <h3>All Staffs</h3>
+                  <h5>Total 10 Users</h5>
+                </div>
+              </div>
+              <div className="pt-3">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Picture</th>
+                      <th>Name</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>001</td>
+                      <td>icon</td>
+                      <td>Jacob</td>
+                    </tr>
+                    <tr>
+                      <td>001</td>
+                      <td>icon</td>
+                      <td>Jacob</td>
+                    </tr>
+                    <tr>
+                      <td>001</td>
+                      <td>icon</td>
+                      <td>Jacob</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
@@ -51,4 +199,7 @@ function Supplier() {
     </div>
   );
 }
-export default Supplier;
+const mapStateToProps = state => ({
+  table: state.table
+});
+export default connect(mapStateToProps)(Suppiler);
