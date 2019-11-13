d0ff955a7831c80be052a64581b8d1b9a0a3520b
Harry <harrysauchhinsun@ymail.com>
2019-11-13 23:48:28 +0700

    update

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import ENV from "../../config/config.json";
 import axios from "axios";
+import { setUser } from "../../actions/user-actions";
 
 import Table from "../../components/Table";
 import face18Jpg from "../../assets/images/faces/face18.jpg";
@@ -11,8 +12,8 @@ function Dashboard(props) {
   const [tableData, setTableData] = useState([]);
   const [responStatus, setResponStatus] = useState(false);
 
-  async function callUserApi() {
-    const response = await axios.get(ENV.API_ENDPOINT + "user", {
+  async function callUserApi(endpoint) {
+    const response = await axios.get(ENV.API_ENDPOINT + endpoint, {
       headers: {
         "x-store": localStorage.getItem(ENV.APP_TOKEN)
       }
@@ -20,10 +21,13 @@ function Dashboard(props) {
     return response;
   }
   useEffect(() => {
-    callUserApi().then(response => {
+    callUserApi("user").then(response => {
       // console.log(response.data.payload);
       setTableData(response.data.payload);
       setResponStatus(true);
+    });
+    callUserApi("staff/profile").then(response => {
+      props.onSetUser(response.data.payload);
     });
     // console.log(data);
   }, []);
@@ -63,9 +67,21 @@ function Dashboard(props) {
                 >
                   {tableData.map(data => (
                     <tr key={data._id}>
-                      <td>{data._id}</td>
-                      <td>{data.username}</td>
-                      <td>{data.role}</td>
+                      <td>
+                        <a href="" className="btn">
+                          <h5>{data._id}</h5>
+                        </a>
+                      </td>
+                      <td>
+                        <a href="" className="btn">
+                          <h5>{data.username}</h5>
+                        </a>
+                      </td>
+                      <td>
+                        <a href="" className="btn">
+                          <h5>{data.role}</h5>
+                        </a>
+                      </td>
                     </tr>
                   ))}
                 </Table>
@@ -93,20 +109,28 @@ function Dashboard(props) {
                   }}
                 ></div>
                 <div>
-                  <h3>Alice Eve</h3>
-                  <h5>Project Manager</h5>
+                  <h3>
+                    {props.user
+                      ? props.user.first_name + " " + props.user.last_name
+                      : "No Name"}
+                  </h3>
+                  <h5>{props.user ? props.user.role : "No Role"}</h5>
                 </div>
               </div>
               <div className="pt-3">
                 <div>
                   <hr />
                   <h5>Email</h5>
-                  <p className="pl-1">aliceeve@gmail.com</p>
+                  <p className="pl-1">
+                    {props.user ? props.user.email : "No Email"}
+                  </p>
                 </div>
                 <div>
                   <hr />
                   <h5>Phone Number</h5>
-                  <p className="pl-1">+(855) 012 554 665</p>
+                  <p className="pl-1">
+                    {props.user ? props.user.email : "No Phone"}
+                  </p>
                 </div>
               </div>
             </div>
@@ -198,7 +222,14 @@ function Dashboard(props) {
     </div>
   );
 }
+const mapActionsToProps = {
+  onSetUser: setUser
+};
 const mapStateToProps = state => ({
-  table: state.table
+  table: state.table,
+  user: state.user
 });
-export default connect(mapStateToProps)(Dashboard);
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(Dashboard);
